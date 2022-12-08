53da5bfeebab80ed910656ac1167537a26009358
kurves <kavssimply@gmailcom>
2022-12-08 20:18:55 +0300

    ínitial commit

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -73,12 +73,60 @@ import { Car, cars as cars_list } from './cars';
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
 
+app.get("/cars/",(req:Request,res:Response)=>{
+
+  
+let {make} = req.query;
+let cars_list=cars;
+
+  if (make) {
+    cars_list = cars.filter((car) => car.make === make);
+  }
+
+ res.status(200)
+.send(cars_list);
+})
+
+
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+app.get("/cars/:id", (req: Request, res:Response)=>{
+
+let {id} = req.params;
+if (!id){
+  return res.status(400)
+  .send(`car of ${id} not found`)
+}
+
+//const car = cars.filter((car)=> car.id === id);
+return res.status(200)
+.send(`car of ${id}`)
+
+})
+
+
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+
+app.post("/cars/", async(req:Request, res:Response)=>{
+
+  let {id, type,make,model,cost} = req.body;
+
+if(!id || !make || !model || !type || !cost){
+  return res.status(400)
+  .send('Car type , name, model and cost is required.')
+}
+
+const new_car : Car ={
+  make:make , type:type, id:id , cost:cost, model:model
+};
+cars.push(new_car);
+return res.status(200)
+.send(`You added car of ${id} ,${type}, ${model} and ksh ${cost}`)
+})
+
 
   // Start the Server
   app.listen( port, () => {
